cf83ba078845788d382cd00e58f7657bffe39ef1
williams <w.chazal@codeur.online>
2021-03-24 16:54:26 +0100

    Commit 1er projet

--- index.php
+++ index.php
@@ -1,6 +1,52 @@
 <?php include "header.php";?>
 
-<div class="container"></div>
+
+<!-- début Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+        <img src="logo.png" id="logo" alt="Le Logo">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <ul class="navbar-nav mr-auto"> <!-- Alignement  à droite-->
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Why Open Entreprise</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contribute</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#"  tabindex="-1" id="font-color" aria-disabled="true">Request early access</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- Fin Navbar -->
+
+
+<div class="container" id="marge-row">
+    <div class="row">
+    
+        <div class="col-sm 6" > <h1> A new model for <br> open collaboration </h1></div>
+       
+
+        <div class="col-sm 6"><img src="Image1.png" id="image1" alt=""></div>
+        
+    
+        
+    </div>
+
+</div>
+
 
 
 
