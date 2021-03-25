4b693bf0c0bd83a4f2a654971e50d6c0663b33a3
williams <w.chazal@codeur.online>
2021-03-25 16:10:31 +0100

    HTML and CSS until half the page

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php include "header.php";?>
 
 
+
+
+
+
+
 <!-- début Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light pb-5">
   <div class="container-fluid">
         <img src="logo.png" id="logo" alt="Le Logo">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,20 +37,86 @@
 
 <!-- Fin Navbar -->
 
-
-<div class="container" id="marge-row">
     <div class="row">
     
-        <div class="col-sm 6" > <h1> A new model for <br> open collaboration </h1></div>
-       
+        <div class="col-sm-12 col-md-6 mt-10"> <h1> A new model for <br> open collaboration </h1>
 
-        <div class="col-sm 6"><img src="Image1.png" id="image1" alt=""></div>
-        
-    
-        
+      <p class="p1 pt-5">Run an organization where members get rewarded for their <br> contributions with fractional ownership.</p>
+      <button type="button" class="btn btn-success bouton">Request early access</button>
+      </div>
+      
+      
+        <div class="col-sm-12 col-md-6"><img src="image personnages.png" id="image1" alt=""></div>
+
     </div>
 
 </div>
+
+<div class="row">
+
+<h2 class="h2 mt-5"> Reimaging what it means to work </h2>
+<p class="p2"> Teams and communities using Open Entreprise fundamentally unlock a reality of work that reimagines how people <br> engage in economic opportuinity, meeting the demands and expectations of a modern organization </p>
+
+<!-- début des cards -->
+
+<div class="card-group">
+  <div class="card">
+    <img src="images-cards/Icon.png" class="card-img-top icons" alt="...">
+    <div class="card-body center-card-title-text">
+    <h5 class="card-title">Modern Workforce</h5>
+      <p class="card-text">Multistakeholder governance aligns employees with the organization’s wider community.</p>
+     
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top icons" alt="...">
+    <div class="card-body center-card-title-text">
+    <h5 class="card-title">Modern Workforce</h5>
+      <p class="card-text">Multistakeholder governance aligns employees with the organization’s wider community.</p>
+      
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top icons" alt="...">
+    <div class="card-body center-card-title-text">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      
+    </div>
+  </div>
+</div>
+
+<!-- Fin des Cards -->
+
+
+<div class="row mt-5">
+
+ <div class="col-sm-12 col-md-6 mt-5 padding-left">
+   <img src="images-cards/icon4.png"> <h2> Kickstart an organization <br> with your co-founders </h2> </div>
+
+ <div class="col-sm-12 col-md-6"> 
+   <img src="images-cards/members-widget.png" id="members-widget-size" class="members-widget size-members-widget" alt="members-widget.png">
+    </div>
+ 
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
