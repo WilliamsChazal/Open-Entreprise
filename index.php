247118364e43e775dd4edea04fb15f8362a3e654
williams <w.chazal@codeur.online>
2021-03-29 16:41:59 +0200

    Commit du 29 mars 2021

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php include "header.php";?>
 
 
-
+<div class="container">
 
 
 
 
 <!-- début Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light pb-5">
-  <div class="container-fluid">
+  <div class="container">
         <img src="logo.png" id="logo" alt="Le Logo">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -50,7 +50,7 @@
 
     </div>
 
-</div>
+
 
 <div class="row">
 
@@ -60,7 +60,7 @@
 <!-- début des cards -->
 
 <div class="card-group">
-  <div class="card">
+  <div class="card p-2">
     <img src="images-cards/Icon.png" class="card-img-top icons" alt="...">
     <div class="card-body center-card-title-text">
     <h5 class="card-title">Modern Workforce</h5>
@@ -68,7 +68,7 @@
      
     </div>
   </div>
-  <div class="card">
+  <div class="card p-2">
     <img src="..." class="card-img-top icons" alt="...">
     <div class="card-body center-card-title-text">
     <h5 class="card-title">Modern Workforce</h5>
@@ -76,7 +76,7 @@
       
     </div>
   </div>
-  <div class="card">
+  <div class="card p-2">
     <img src="..." class="card-img-top icons" alt="...">
     <div class="card-body center-card-title-text">
       <h5 class="card-title">Card title</h5>
@@ -92,19 +92,117 @@
 <div class="row mt-5">
 
  <div class="col-sm-12 col-md-6 mt-5 padding-left">
-   <img src="images-cards/icon4.png"> <h2> Kickstart an organization <br> with your co-founders </h2> </div>
+   <img src="images-cards/icon4.png"> <h2> Kickstart an organization <br> with your co-founders </h2> 
+   <p>It’s the early days, you just had a long conversation with two friends about a meaningful <br>
+    challenge that you’re all passionate about and have a potential solution for. You’re ready to embark the startup journey.</p>
+  </div>
 
- <div class="col-sm-12 col-md-6"> 
-   <img src="images-cards/members-widget.png" id="members-widget-size" class="members-widget size-members-widget" alt="members-widget.png">
+  <div class="col-sm-12 col-md-6"> 
+    <div class="card mt-5" id="card-member-size">
+      <div class="card-body border-dark border rounded box-shadow">
+        <div class="row">
+          <div class="col-12">Members</div>
+          <div class="col-3">
+            <img src= dillon.png>
+          </div>
+          <div class="col-9">
+            <div class="row">
+              <div class="col-12"><p> Dillon Kydd</p></div>
+              <div class="col-12"><p>100 SHARES</p></div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-3">
+            <img src= anna.png>
+          </div>
+          <div class="col-9">
+            <div class="row">
+              <div class="col-12"><p> Dillon Kydd</p></div>
+              <div class="col-12"><p>100 SHARES</p></div>
+            </div>
+          </div>
+        </div>
+  <!-- Ligne kas -->
+        <div class="row">
+          <div class="col-3">
+            <img src= kas.png>
+          </div>
+          <div class="col-9">
+            <div class="row">
+              <div class="col-12"><p> Dillon Kydd</p></div>
+              <div class="col-12"><p>100 SHARES</p></div>
+            </div>
+          </div>
+        </div>
+  <!-- Fin ligne Kas -->
+      </div>
     </div>
- 
+  </div>
+</div>
 
+<!-- Jassir -->
+<div class="row">
+  <div class="col-md-4">
+  <div class="card p-3 border-dark">
+  <div class="card-body">
+    <div class="row">
+      <div class="col-6">
+         <img src=jassir.png style="width 72px">
+    </div>
+      <div class="col-6">
+        <div class="row">
+         <div><h5 class="card-title col-12 ml-5">Card title</h5></div>
+         <div><h6 class="card-subtitle mb-2 text-muted col-12">Card subtitle</h6></div>
+      
+         
+         </div>
+       </div>
+    </div>
+    <div class="row">
+         <div><p class="card-text col-12">Some quick example text to build on the card title and make up the bulk of the card's content.</p></div>
+  <div class="row">
+  <div class="col-6"> COMMIT </div>
+  <div class="col-6"> SHARES </div>
+  </div>
 
+  <div class="row">
+  <div class="col-6"> 10,000 DAI </div>
+  <div class="col-6"> 20  </div>
+  </div>
 
+  <div class="row">
+  <div class="d-grid gap-2"> <button type="button" class="btn btn-dark">Go to vote</button> </div>
 
+  </div>
+
+  </div>
+  </div>
+  </div>
+  <div class="col-md-2"></div>
+  <!-- Attention: doit rester vide ! -->
+  
+  
+  <!-- mon html ici -->
+  <div class="col-md-10">
+      <div class="row">
+        <div class="col-3"><img src=images-cards/icon.png></div>
+        <div class="row"><h3>Onboard users, investors and advisors as you grow</h3></div>
+      </div>
+  </div>
+
+  </div>
+  
 </div>
 
 
+
+                    
+              
+  
+
+
+          
 
 
 
