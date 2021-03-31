b23f7682781c51bc93d40c0093893f482149b6f2
williams <w.chazal@codeur.online>
2021-03-31 15:15:03 +0200

    update

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 
     <div class="row">
     
-        <div class="col-sm-12 col-md-6 mt-10"> <h1> A new model for <br> open collaboration </h1>
+        <div class="col-sm-12 col-md-6 mt-10"> <h1 class="title"> A new model for <br> open collaboration </h1>
 
       <p class="p1 pt-5">Run an organization where members get rewarded for their <br> contributions with fractional ownership.</p>
       <button type="button" class="btn btn-success bouton">Request early access</button>
@@ -54,33 +54,35 @@
 
 <div class="row">
 
+<div class="row mt-3"><img src="divider.png"></div>
+
 <h2 class="mt-5"> <h2>Reimaging what it means to work </h2>
 <p class="p2"> Teams and communities using Open Entreprise fundamentally unlock a reality of work that reimagines how people <br> engage in economic opportuinity, meeting the demands and expectations of a modern organization </p>
 
 <!-- début des cards -->
 
-<div class="card-group color-background-cards">
-  <div class="card p-2">
+<div class="card-group">
+  <div class="card p-2 m-2">
     <img src="images-cards/icon.png" class="card-img-top icons" alt="...">
-    <div class="card-body center-card-title-text">
-    <h5 class="card-title">Modern Workforce</h5>
+    <div class="card-body center-card-title-text  color-background-cards">
+    <h4 class="card-title">Modern Workforce</h4>
       <p class="card-text">Multistakeholder governance aligns employees with the organization’s wider community.</p>
      
     </div>
   </div>
-  <div class="card p-2">
+  <div class="card p-2 m-2">
     <img src="images-cards/imagecards2.png" class="card-img-top icons" alt="...">
-    <div class="card-body center-card-title-text">
-    <h5 class="card-title">Modern Workforce</h5>
-      <p class="card-text">Multistakeholder governance aligns employees with the organization’s wider community.</p>
+    <div class="card-body center-card-title-text  color-background-cards">
+    <h4 class="card-title">Meritocratic by design</h4>
+      <p class="card-text">Tokenized ownership aligns deeply committed individuals with the organization’s success.</p>
       
     </div>
   </div>
-  <div class="card p-2">
+  <div class="card p-2 m-2">
     <img src="images-cards/imagecards3.png" class="card-img-top icons" alt="...">
-    <div class="card-body center-card-title-text">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+    <div class="card-body center-card-title-text  color-background-cards">
+      <h4 class="card-title">Engineered for resilience</h4>
+      <p class="card-text">Open Enterprise supports best practices in sociocratic management.
       
     </div>
   </div>
@@ -88,6 +90,7 @@
 
 <!-- Fin des Cards -->
 
+<div class="row mt-3"><img src="divider.png"></div>
 
 <div class="row mt-5">
 
@@ -97,18 +100,18 @@
     challenge that you’re all passionate about and have a potential solution for. You’re ready to embark the startup journey.</p>
   </div>
 
-  <div class="col-sm-12 col-md-6"> 
+  <div class="col-sm-12 col-md-4 me-5"> 
     <div class="card mt-5" id="card-member-size">
       <div class="card-body border-dark border rounded box-shadow">
         <div class="row">
-          <div class="col-12">Members</div>
+          <div class="col-12"><h5>Members</h5></div>
           <div class="col-3">
             <img src= dillon.png>
           </div>
           <div class="col-9">
             <div class="row">
-              <div class="col-12"><p> Dillon Kydd</p></div>
-              <div class="col-12"><p>100 SHARES</p></div>
+              <div class="col-12">Dillon Kydd</div>
+              <div class="col-12">100 SHARES</div>
             </div>
           </div>
         </div>
@@ -118,8 +121,8 @@
           </div>
           <div class="col-9">
             <div class="row">
-              <div class="col-12"><p> Dillon Kydd</p></div>
-              <div class="col-12"><p>100 SHARES</p></div>
+              <div class="col-12">Anna Olsen</div>
+              <div class="col-12">100 SHARES</div>
             </div>
           </div>
         </div>
@@ -130,8 +133,8 @@
           </div>
           <div class="col-9">
             <div class="row">
-              <div class="col-12"><p> Dillon Kydd</p></div>
-              <div class="col-12"><p>100 SHARES</p></div>
+              <div class="col-12">Kas Moss</div>
+              <div class="col-12">100 SHARES</div>
             </div>
           </div>
         </div>
@@ -140,6 +143,8 @@
     </div>
   </div>
 </div>
+
+<div class="row mt-3"><img src="divider.png"></div>
 
 <!-- Jassir -->
 
@@ -157,19 +162,19 @@
                   </div>
                 </div>     
               <div class="row mt-2">
-                <div class="col-12"> <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></div>
+                <div class="col-12 card1">Some quick example text to build on the card title and make up the bulk of the card's content.</div>
               </div> 
                 <div class="row mt-2">
-                  <div class="col-6"><p>COMMIT</p></div>
-                  <div class="col-6"><p>SHARES</p></div>
+                  <div class="col-6">COMMIT</div>
+                  <div class="col-6">SHARES</div>
                  </div>
                     <div class="row">
-                      <div class="col-6"><p>10,000 DAI</p></div>
-                      <div class="col-6"><p>20</p></div>
+                      <div class="col-6">10,000 DAI</div>
+                      <div class="col-6">20</div>
                     </div>
                  <div class="row mt-4 mb-3">
                  <div class="d-grid gap-2">
-                     <button class="btn btn-dark" type="button">Go to vote</button></div>
+                     <button class="btn btn-dark mx-auto" type="button">Go to vote</button></div>
                  </div>
         </div>
 
@@ -195,8 +200,9 @@
 
   <!-- fin § Onboard users -->
 
+  <div class="row mt-3"><img src="divider.png"></div>
 
-  <!-- début § Onboard engage -->
+  <!-- début § engage -->
 
  <div class="row mt-5">
       <div class="col-sm-12 col-md-6 mt-3">
@@ -210,11 +216,11 @@
               <div class="col-12"><p>You need talented people on-demand across various tasks that your full-time workforce can’t prioritize at the moment. As an Open Enterprise, you can easily fund tasks and have people apply to work on them in return for funds or shares.</p></div>
            </div>
       </div>
-
+     
   <!-- ENcardé  -->
 
       <!-- ENcardé Write -->
-        <div class="col-sm-12 col-md-6 mt-3 padding-left margin-right">
+        <div class="col-sm-12 col-md-6 mt-3 padding-left margin-right ps-3">
             <div class="row border-dark border rounded box-shadow-bottom">
               <div class="col-2"></div>
                   <div class="col-8">
@@ -233,7 +239,7 @@
                         <div class="col-7 border-dark border rounded background-color">
                       <div class="row">
                           <div class="col-3"></div>
-                          <div class="col-9">3 applicants</div>
+                          <div class="col-9"><h5>3 applicants</h5></div>
                       </div>
                       <div class="row">
                         <div class="col-3"><img src="jason.png"></div>
@@ -253,20 +259,21 @@
 
                         </div>
                     <div class="col-1"></div>
+
             <!-- Card Alex Rose -->
-                    <div class="col-4 sm-12 md-6 row border-card-green rounded">
+                    <div class="col-md-4 col-sm-12 row border-card-green rounded mt-3 margin-left">
                             <div class="row mt-2">
                                     <div class="col-4"></div>
                                     <div class="col-4"><img src="alex.png"></div>
                                     <div class="col-4"></div>
                             </div>
                         
-                        <div class="col-12 ">
+                        <div class="col-md-12 col-sm-12 ">
                              <div class="row"><p class="text-center">Alex Rose <br> UX Writer</p></div>
                                 <div class="row mt-3">
                                     <div class="col-1 ml-1">4</div>
-                                    <div class="col-6"><p class="text-left">COMMITS</p></div>
-                                    <div class="col-2"><p>4.5</p></div>
+                                    <div class="col-6 pe-5"><p>COMMITS</p></div>
+                                    <div class="col-2 ps-1"><p>4.5</p></div>
                                     <div class="col-2"><img src="star.png"></div>
                                 </div>
                         </div> 
@@ -276,7 +283,9 @@
                     </div>
            </div>
         <!-- ENcardé  -->
- </div>               
+ </div>  
+
+ 
 
       <!-- Dernier § -->
       <div class="row mt-5"><img src="divider.png"></div>
@@ -287,24 +296,24 @@
       </div>
     <!-- fin Dernier § -->
     
-    
+    <div class="row mt-3"><img src="divider.png"></div>
 
 <!-- Card bas de page -->
-    <div class="col-12 md-12 sm-6 mt-5">
+    <div class="col-md-12 col-sm-6 mt-5">
               <div class="row">
-                  <div class="col-3  md-12 sm-6  card-bottom-shadow offset-1 border rounded">
-                      <div class="row padding-left">Ideate a liste of features based on the Sociocracy model</div>
-                      <div class="row padding-left pt-2">We're looking for someone interested in buisness theroy and research that'll help us bridge the gap between our s...</div>
+                  <div class="col-md-3 col-sm-12 card-bottom-shadow offset-1 border rounded">
+                      <div class="row padding-left"><h5>Ideate a liste of features based on the Sociocracy model</h5></div>
+                      <div class="row ps-2 pt-2">We're looking for someone interested in buisness theroy and research that'll help us bridge the gap between our s...</div>
                       <div class="row">
-                            <div class="col-6 pt-2 justify-content-left"><button type="button" class="btn btn-success rounded-pill">5 AUT</button></div>
+                            <div class="col-6 ps-2 pt-2 justify-content-left"><button type="button" class="btn btn-success rounded-pill">5 AUT</button></div>
                             <div class="col-6 justify-content-right pt-3">Apply by may 15</div>
                       </div>
                   </div>
 
-                <div class="col-3  md-12 sm-6  offset-1 card-bottom-shadow  border rounded">
+                <div class="col-md-3 col-sm-12 offset-1 card-bottom-shadow  border rounded">
                       <div class="row pt-2">
-                          <div class="row padding-left">Update documentation with FAQs</div>
-                          <div class="row padding-left pt-2">Review our communtiy chat for the most frequently asked questions and document answers four our product docs.</div>
+                          <div class="row padding-left"><h5>Update documentation with FAQs</h5></div>
+                          <div class="row  ps-3">Review our communtiy chat for the most frequently asked questions and document answers four our product docs.</div>
                             <div class="row">
                               <div class="col-6 pt-3"> <button type="button" class="btn btn-success rounded-pill">10 AUT</button></div>
                               <div class="col-6 justify-content-right pt-3">Apply by May 18</div>
@@ -312,14 +321,15 @@
                         </div>
                 </div>
 
-                  <div class="col-3   md-12 sm-6 card-bottom-shadow offset-1 border rounded">
-                        <div class="row padding-left"> Tutorial video series</div>
-                        <div class="row  padding-left pt-2">Create a series of video tutorial that cover everything from start to finish using Open Entreprise</div>
+                  <div class="col-md-3  col-sm-12 card-bottom-shadow offset-1 border rounded">
+                        <div class="row padding-left"><h5>Tutorial video series</h5></div>
+                        <div class="row ps-2 pt-2">Create a series of video tutorial that cover everything from start to finish using Open Entreprise</div>
                       <div class="row">
                           <div class="col-6 pt-3"><button type="button" class="btn btn-success rounded-pill">40 AUT</button></div>
                           <div class="col-6 justify-content-right pt-3"> Apply by May 20</div>
                       </div>         
-               </div>
+             </div>
+
             <!-- Fin cards bottom -->
     <div class="row mt-5">
       <div class="col-4"></div>
@@ -332,108 +342,25 @@
 
     <!-- § et image bottom -->
 
-        <div class="row  md-12 sm-6 ">
-            <div class="col-6  md-12 sm-6 ">
+        <div class="row">
+            <div class="col-sm-12 col-md-6 ml-5">
               <div class="row pt-3"><h2>Start an Open Entreprise</h2></div>
               <div class="row pt-3"><p>If you can’t wait to run a new or existing organization on Open Enterprise and are willing to explore and navigate the beta, we’d love to get you started.</p></div>
               <div class="row"> <button type="button" class="btn btn-success bouton">Request early access</button></div>
             </div>
-            <div class="col-6 col-sm-12 col-md-6 "><img src="pablo.png" id="image1"></div>
+            <div class="col-sm-12 col-md-6 "><img src="pablo.png" id="image2"></div>
         </div>
 
         <div class="row">
           <div class="col-sm-12 col-md-6 ">
           <p>Open Entreprise</p></div> 
-          <div class="col-sm-12 col-md-6 "><p class="text-end"> Documentation Github Twitter <p class="font-color">Contact us</p></div>
+          <div class="col-sm-12 col-md-6 "><div><p class="text-right"> Documentation Github Twitter</div><p id="font-color">Contact us</p></div>
         </div>
                     
 
-                    
-
-                          
-                  
-              
-    
-
-
-
+     
     </div>
     
-
-           
-            
-           
-
-           
-    
-           
-
-
-
-    
-
-       
-
-
-
-                 
-               
-               
-                 
-                    
-                         
-
-
-   
-
-
-
-
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    
-              
-  
-
-
-          
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <?php include "footer.php";?>
 
